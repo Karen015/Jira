51d4698924f187e7dcd1cc6af7651bf1def8ce68
Karen015 <karen.aghajanyan.11@gmail.com>
2024-07-31 20:15:59 +0400

    fixing Header import

--- src/view/layouts/MainLayout/index.js
+++ src/view/layouts/MainLayout/index.js
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
-
-import Header from '../../components/global/Header';
+import { Header } from '../../components/global';
 import './index.css';
 
 const MainLayout = () => {
