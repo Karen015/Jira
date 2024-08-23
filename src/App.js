3ea297a6051eeb62b05f6b4d789ee7cb502b0038
Karen015 <karen.aghajanyan.11@gmail.com>
2024-08-23 12:27:55 +0400

    implement redirect functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import { useState, useEffect } from 'react';
 import { MainLayout, CabinetLayout } from './view/layouts';
 import { Login, Register } from './view/pages/auth';
+import CabinetBoard from './view/pages/cabinetBoard';
 import LoadingWrapper from './view/components/shared/LoadingWrapper';
 import { db, auth, doc, getDoc, onAuthStateChanged } from './services/firebase/firebase';
 import { AuthContextProvider } from './context/AuthContext';
+import { ROUTES_CONSTANTS } from './routes';
 import {  
   Route, 
+  Navigate,
   RouterProvider,
-  createHashRouter, 
+  createBrowserRouter, 
   createRoutesFromElements,
-  redirect
 } from 'react-router-dom';
 import './App.css';
 
-const route = createHashRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-        <Route path="login" element={<Login />}/>
-        <Route path="register" element={<Register />}/>
 
-        <Route path="cabinet" element={<CabinetLayout />}>
-
-        </Route>  
-    </Route>
-  )
-)
 
 const App = () => {
   const [isAuth, setIsAuth] = useState(false);
@@ -37,36 +28,57 @@ const App = () => {
   });
 
   useEffect(() => {
-    setLoading(true)
-    onAuthStateChanged(auth, (user) => {          
-      setLoading(false);
+    setLoading(true);
+    
+    onAuthStateChanged(auth, (user) => { 
+      setLoading(false)
 
-      if (user) {         
+      if (user) {
         setIsAuth(true);
-        const { uid } = user;
-        const ref = doc(db, 'registerUsers', uid);
+          const { uid } = user;
+          const ref = doc(db, 'registerUsers', uid);
 
-        getDoc(ref).then((userData) => {
-          if (userData.exists()) {
-            setUserProfileInfo(userData.data())
-          }
-        })
+          getDoc(ref).then((userData) => {
+            if (userData.exists()) {
+              setUserProfileInfo(userData.data()) 
+            }
+          })
       } else {
-        redirect('/login')
+
       }
     })
   }, [])
 
   return (
     <LoadingWrapper loading={loading} fullScreen>
-      <AuthContextProvider value={{ isAuth, userProfileInfo, setIsAuth}}>
-        <RouterProvider router={route}/>
+      <AuthContextProvider value={{ isAuth, userProfileInfo, setIsAuth }}>
+        <RouterProvider router={
+          createBrowserRouter(
+            createRoutesFromElements(
+              <Route path="/" element={<MainLayout />}>
+                  <Route 
+                    path={ROUTES_CONSTANTS.LOGIN} 
+                    element={!isAuth ? <Login /> : <Navigate to={ROUTES_CONSTANTS.CABINET}/>}
+                  />
+                  <Route 
+                    path={ROUTES_CONSTANTS.REGISTER} 
+                    element={!isAuth ? <Register /> : <Navigate to={ROUTES_CONSTANTS.CABINET}/>}
+                  />
+
+                  {/* ------ Cabinet Layout Route ------ */}
+                  <Route 
+                    path={ROUTES_CONSTANTS.CABINET} 
+                    element={isAuth ? <CabinetLayout /> : <Navigate to={ROUTES_CONSTANTS.LOGIN}/>} 
+                  >
+                    <Route path={ROUTES_CONSTANTS.CABINET} element={<CabinetBoard />}/>
+                  </Route>
+              </Route>
+            )
+          )
+        }/>
       </AuthContextProvider>
     </LoadingWrapper>
   )
-}
+};
 
 export default App;
-
-
-
